62263ac8ff42b8ad2b5725445e8246c69e28caa6
eLAmaravati <amaravatilangit@gmail.com>
2022-12-11 02:47:38 +0700

    add google tag manager

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -2,26 +2,22 @@ import Head from "next/head";
 import Navbar from "../Navbar";
 import { ReactNode } from "react"
 import Footer from "../Footer"
-import Script from "next/script";
 
 interface LayoutProps {
-    children: ReactNode;
-    pageTitle: string;
+	children: ReactNode;
+	pageTitle: string;
 }
 export default function Layout(props: LayoutProps) {
-    const { children, pageTitle } = props;
-    return (
-        <>
-            <Head>
-                <title>{pageTitle} | Easybank landing page</title>
-                <meta name="description" content="A solution to the Easybank landing page challenge on Frontend Mentor" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico" />
-
-                {/* Google Tag */}
-                <script async src="https://www.googletagmanager.com/gtag/js?id=G-9XYMH7M46P" />
-            </Head>
-            <Navbar />
-            <main className="main">{children}</main>
-            <Footer /></>
-    )
+	const { children, pageTitle } = props;
+	return (
+		<>
+			<Head>
+				<title>{pageTitle} | Easybank landing page</title>
+				<meta name="description" content="A solution to the Easybank landing page challenge on Frontend Mentor" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico" />
+			</Head>
+			<Navbar />
+			<main className="main">{children}</main>
+			<Footer /></>
+	)
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,30 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 const pubclicSans = Public_Sans({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  
+
   return <>
     <style jsx global>{`
         html {
           font-family: ${pubclicSans.style.fontFamily};
         }
       `}</style>
+
+    <Script strategy='afterInteractive' src="https://www.googletagmanager.com/gtag/js?id=G-9XYMH7M46P" />
+    <Script id='google-analytics' strategy='afterInteractive'>
+      {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-9XYMH7M46P');
+      `}
+    </Script>
+
     <Component {...pageProps} />
   </>
 }
